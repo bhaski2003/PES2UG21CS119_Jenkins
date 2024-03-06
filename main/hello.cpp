efdc0d7b38774a352a5142d5bd42fa38a739f695
Bhaskar KR <95578571+bhaski2003@users.noreply.github.com>
2024-03-06 12:29:24 +0530

    Update hello.cpp
    missed ';' --> fr pain in a**

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   // cout << "Hello, World\n";
   // cout << "Hello, Jenkins\n";
   // cout << "I have successfully built and run\n";
-  cout << "Hi , there I'm SRN - PES2UG21CS119 , \n have added the superHero WEBHOOK --> ngrok now , im testing it :) "
-  cout << "And I'm veery exited to test this AUTOMATION PROCESS XD"
+  cout << "Hi , there I'm SRN - PES2UG21CS119 , \n have added the superHero WEBHOOK --> ngrok now , im testing it :) ";
+  cout << "And I'm veery exited to test this AUTOMATION PROCESS XD";
   return 0;
 }
