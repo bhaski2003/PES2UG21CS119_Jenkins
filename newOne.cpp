e21b1fad5c0632b47f8c99fa0af01926d38f057d
Bhaskar KR <95578571+bhaski2003@users.noreply.github.com>
2024-03-06 14:50:39 +0530

    Create newOne.cpp
    have created a new file to test my pipline

---
+++ newOne.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  
+    std::cout << "Hi , I'm PES2UG21CS119" << std::endl;
+    std::cout << "Have coded a PipeLine for ease in CI/CD and this file is added and \n 
+                 the triggered by webHook and compiled , built , tested & been executed via PipeLine as a result of it" << std::endl;
+    std::cout << "1 \n 2 \n 3 \n 4" << std::endl; 
+  
+  return 0;
+}
